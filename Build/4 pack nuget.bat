67a87dbe556f9def4f8cc0f9de9e0ccccac477c3
Frank Dekker <fdekker@123inkt.nl>
2020-12-21 20:26:12 +0100

    Minimal package requirements for html rendering in WPF

--- Build/4 pack nuget.bat
+++ Build/4 pack nuget.bat
@@ -12,7 +12,4 @@ echo.
 echo - Pack NuGets...
 echo.
 nuget.exe pack NuGet\HtmlRenderer.Core.nuspec -Version %version% -OutputDirectory Release
-nuget.exe pack NuGet\HtmlRenderer.WinForms.nuspec -Version %version% -OutputDirectory Release
 nuget.exe pack NuGet\HtmlRenderer.WPF.nuspec -Version %version% -OutputDirectory Release
-nuget.exe pack NuGet\HtmlRenderer.Mono.nuspec -Version %version% -OutputDirectory Release
-nuget.exe pack NuGet\HtmlRenderer.PdfSharp.nuspec -Version %version% -OutputDirectory Release
